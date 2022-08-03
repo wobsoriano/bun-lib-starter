38c99a0690a1f0a9b660bb56f67cb0fc7d20ed84
Robert Soriano <sorianorobertc@gmail.com>
2022-08-03 00:03:40 -0700

    fix entry loc

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-  entry: ['lib/index.ts'],
+  entry: ['src/index.ts'],
   clean: true,
   format: ['cjs', 'esm'],
   external: ['bun', 'bun:ffi', 'bun:jsc', 'bun:sqlite'],
