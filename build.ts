393be1aa3e240cba694b8a55f93acf4cb0bb1314
wobsoriano <sorianorobertc@gmail.com>
2023-05-19 13:18:55 -0700

    use built-in bundler

---
+++ build.ts
@@ -0,0 +1,7 @@
+await Bun.build({
+  entrypoints: ['./src/index.ts'],
+  outdir: './dist',
+  minify: true,
+})
+export { };
+
--- tsup.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'tsup';
-
-export default defineConfig({
-  entry: ['src/index.ts'],
-  clean: true,
-  format: ['cjs', 'esm'],
-  external: ['bun', 'bun:ffi', 'bun:jsc', 'bun:sqlite'],
-  dts: true,
-});
