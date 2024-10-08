13aef39efb2377de5141b1ded1a7b0fc79abc88d
wobsoriano <sorianorobertc@gmail.com>
2024-10-08 10:50:47 -0700

    add cjs support

---
+++ build.ts
@@ -0,0 +1,22 @@
+import type { BuildConfig } from 'bun'
+import dts from 'bun-plugin-dts'
+
+const defaultBuildConfig: BuildConfig = {
+  entrypoints: ['./src/index.ts'],
+  outdir: './dist',
+  minify: true
+}
+
+await Promise.all([
+  Bun.build({
+    ...defaultBuildConfig,
+    plugins: [dts()],
+    format: 'esm',
+    naming: "[dir]/[name].js",
+  }),
+  Bun.build({
+    ...defaultBuildConfig,
+    format: 'cjs',
+    naming: "[dir]/[name].cjs",
+  })
+])
